2360abe49b8e4a6608863bc280e75abf0fcd7494
LisaF0 <lisaforet71@gmail.com>
2020-12-08 12:14:18 +0100

    dailyPush V1
    Stripe ok
    TODO config price and product

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -2,19 +2,20 @@
 
 namespace App\Controller;
 
+use Stripe\Product;
 use App\Entity\Cart;
 use Stripe\StripeClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CheckoutController extends AbstractController
 {
     /**
-     * @Route("/checkout", name="checkout")
+     * @Route("/index", name="index")
      */
     public function index(SessionInterface $session): Response
     {
@@ -27,9 +28,12 @@ class CheckoutController extends AbstractController
             ];
         }
         $total = $cart->getTotal($incart);
+
         $stripe = new \Stripe\StripeClient(
             'sk_test_51HvgjELyEjuAwgbZtFkkq4UfxmsjafIAB10xIVuEjqHkQqVuHmrtBD4XvNGHPLnsOc7cKV8eL2lFxVNnVNSgyfpv00TCqiAFXL'
           );
+
+
           $stripe->checkout->sessions->create([
             'success_url' => $this->generateUrl('success', [], UrlGeneratorInterface::ABSOLUTE_URL),
             'cancel_url' => $this->generateUrl('error', [], UrlGeneratorInterface::ABSOLUTE_URL),
@@ -50,6 +54,35 @@ class CheckoutController extends AbstractController
             'mode' => 'payment',
           ]);
         return new JsonResponse([ 'id' => $session->getId() ]);
+    }
+
+    /**
+     * @Route("/create-checkout-session", name="checkout")
+     */
+    public function checkout()
+    {
+        \Stripe\Stripe::setApiKey('sk_test_51HvgjELyEjuAwgbZtFkkq4UfxmsjafIAB10xIVuEjqHkQqVuHmrtBD4XvNGHPLnsOc7cKV8eL2lFxVNnVNSgyfpv00TCqiAFXL');
+       
+
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [[
+              'price_data' => [
+                'currency' => 'usd',
+                'product_data' => [
+                  'name' => 'T-shirt',
+                ],
+                'unit_amount' => 2000,
+              ],
+              'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'success_url' => $this->generateUrl('success', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('error', [], UrlGeneratorInterface::ABSOLUTE_URL),
+          ]);
+
+        return new JsonResponse([ 'id' => $session->id ]);
+
     }
 
     /**
